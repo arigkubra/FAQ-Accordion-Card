18f4688aa3bcaf2c30db64c583c17dbbdc4ff0ed
arigkubra <arigkubra@gmail.com>
2023-08-05 15:33:34 +0300

    button rotation added

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const faqs = document.querySelectorAll('.faq');
+const arrowPicture = document.querySelector('.arrowPicture');
+
 
 faqs.forEach(faq => {
   faq.addEventListener('click', () => {
     const answer = faq.querySelector('.answer');
     answer.style.display = answer.style.display === 'block' ? 'none' : 'block';
+
   });
 });
+
+arrowPicture.forEach(arrow => {
+    arrow.addEventListener('click', () => {
+        const image = arrow.querySelector('.img')
+        image.style.transform = image.style.transform === 'rotate(0deg)' ? 'rotate(180deg)' : 'rotate(0deg)';
+    });
+});
+
 
